adad295c59a5498d1e326fe12da4f5035714951b
ghostuf <srijan102dahal@gmail.com>
2024-01-28 13:30:57 +0545

    Add: custom not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function Notfound(){
+    return(
+        <>
+            <h1>Bruh what you lookin at</h1>
+            <p>Couldnt find the requested resource!</p>
+        </>
+    )
+}
