303d3caa92f47c1a05089420f6f6984bb09d53ac
ghostuf <srijan102dahal@gmail.com>
2024-01-28 13:30:23 +0545

    Modified: rewiews page added not found condition

--- src/app/products/[productID]/reviews/[reviewID]/page.jsx
+++ src/app/products/[productID]/reviews/[reviewID]/page.jsx
@@ -1,4 +1,15 @@
-export default function Reciews({params}){ // props = ["prams", ""]
+import Notfound from "@/app/not-found.jsx"
+
+export default function Reviews({params}){ // props = ["prams", ""]
+
+    if(parseInt(params.reviewID) > 1000){
+        return(
+            <div>
+                <Notfound />
+            </div>
+        )
+    }
+
     return(
         <>
             <h1>Product {params.productID}: reviews: {params.reviewID}</h1>
