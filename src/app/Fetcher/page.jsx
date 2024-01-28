1c5b50d404d276592758cd54c9401158eb59d766
ghostuf <srijan102dahal@gmail.com>
2024-01-28 21:43:26 +0545

    Change: Added more fetching and Navbar

--- src/app/Fetcher/page.jsx
+++ src/app/Fetcher/page.jsx
@@ -1,32 +1,54 @@
 "use client";
+import Navbar from "../Navbar";
 import { useState } from "react";
 
 export default function Fetched() {
   const [input, inputchanger] = useState("");
 
   async function Fetcher() {
+    const animres = await fetch(`https://api.jikan.moe/v4/anime/${input}/full`);
     const res = await fetch(
       `https://api.jikan.moe/v4/anime/${input}/characters`
     );
     const newdata = await res.json();
+    const newanim = await animres.json();
 
+    const animName = newanim.data.titles[0].title;
     const charName = newdata.data[0].character.name;
     const charimg = newdata.data[0].character.images.jpg.image_url;
-    const divElem = document.querySelector(".anime-container");
+    const divElem = document.querySelector("#anime-container");
 
-    divElem.innerHTML = `<p>character name: ${charName}</p><br><img src = "${charimg}">`;
+    divElem.innerHTML = `<div><p>Character Name : ${charName}</p><br><p>Anime/Movie : ${animName}</p></div><div><img src = "${charimg}"><div>`;
   }
 
-    return (
-        <div className="container">
+  return (
+    <div>
+      <Navbar />
+
+      <main className="flex flex-col items-center mt-14 gap-14">
+        <div className=" text-purple-400 font-mono text-3xl">
+          Search your Anime character based on a id provided from MyAnimelist or
+          explore by inputting random Ids.
+        </div>
+        <div className=" flex flex-row gap-2">
           <input
             type="text"
             value={input}
             onChange={(e) => inputchanger(e.target.value)}
+            className=" w-96 h-10 p-2 rounded"
           ></input>
-          <button onClick={Fetcher}>Click Me</button>
-    
-          <div className="anime-container"></div>
+          <button
+            onClick={Fetcher}
+            className="text-white bg-violet-500 hover:bg-violet-600 p-2 w-50 rounded"
+          >
+            Click Me
+          </button>
         </div>
-      );
+        <div
+          id="anime-container"
+          className="text-white flex flex-row gap-8 text-3xl"
+        ></div>
+      </main>
+    </div>
+  );
 }
